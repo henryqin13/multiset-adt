ab3074402da4fc4dc6df4ee6c7bae62828d66c1a
Chenxu Robin Mao <robin.mao@mail.utoronto.ca>
2023-09-18 16:22:08 -0400

    finished the initializer and the testing

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tree {
+    // Object is the <Any> type in Java
+    private Object root;
+    private List<Tree> subtrees;
+
+    public Tree(Object root) {
+        this.root = root;
+        // no subtrees provided
+        this.subtrees = new ArrayList<>();
+    }
+
+    public Tree(Object root, ArrayList<Tree> subtrees){
+        this.root = root;
+        // subtrees provided
+        this.subtrees = subtrees;
+    }
+
+    public boolean is_empty(){
+        return this.root == null;
+    }
+
+//    public static void main(String[] args) {
+//        Tree t1 = new Tree("123");
+//        System.out.println(t1.is_empty());
+//    }
 }
